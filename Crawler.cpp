e00b8facd8ed6282d1b17301bccef85ab97bee76
caitlinmag <caitlinmaguire14@outlook.com>
2024-04-06 15:45:26 +0100

    Created crawler and hopper bug class and header class.
    Also created function to display each bug in the text file on a new line, instead of how it was displaying all bugs on one line. Created a vector of pointers to bug objects, next step is populating the vector with the contents of the text file.

---
+++ Crawler.cpp
@@ -0,0 +1,11 @@
+//
+// Created by caitl on 06/04/2024.
+//
+
+#include "Crawler.h"
+#include <iostream>
+using namespace std;
+
+void Bug::move() {        //implementation of Crawler move()
+cout << "Moving a crawler bug " << endl;
+}
